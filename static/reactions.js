47b4bdb891d44c58aa1f152c25525b54a7f01b62
Alisa Kolot <kolot.alisa@gmail.com>
2020-08-26 22:37:14 -0500

    toggle radio button

--- static/reactions.js
+++ static/reactions.js
@@ -1,2 +1,7 @@
-//Handling Reactions//
+// "use-strict";
 
+// const button1 = document.getElementById("reaction-1");
+
+// button.addEventListener('click', () => {
+//   alert('Stop clicking me!');
+// });
